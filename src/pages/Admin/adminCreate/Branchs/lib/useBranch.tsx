4b3a8db7140e2953a9fe31ca89f1200c45ba2ac1
baoha7204 <baoha3604@gmail.com>
2024-06-12 12:48:04 +0700

    [DEBO04][BaoHMQ] Fix conflict

--- src/pages/Admin/adminCreate/Branchs/lib/useBranch.tsx
+++ src/pages/Admin/adminCreate/Branchs/lib/useBranch.tsx
@@ -13,68 +13,67 @@ import { useNavigate } from "react-router-dom";
 export type BranchInputs = z.infer<typeof branchSchema>;
 
 export default function useBranch() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const {
-        handleSubmit,
-        reset,
-        control,
-        formState: { isSubmitSuccessful, isSubmitting },
-    } = useForm<BranchInputs>({
-        resolver: zodResolver(branchSchema),
-        defaultValues: {
-            id: 0,
-            // branchAvt: [],
-            name: '',
-            address: '',
-            phone: '',
-            email: '',
-        },
-    });
+  const {
+    handleSubmit,
+    reset,
+    control,
+    formState: { isSubmitSuccessful, isSubmitting },
+  } = useForm<BranchInputs>({
+    resolver: zodResolver(branchSchema),
+    defaultValues: {
+      id: 0,
+      // branchAvt: [],
+      name: "",
+      address: "",
+      phone: "",
+      email: "",
+    },
+  });
 
-    console.log('0');
+  console.log("0");
 
-    const onSubmit: SubmitHandler<BranchInputs> = (data) => {
+  const onSubmit: SubmitHandler<BranchInputs> = (data) => {
+    console.log("1");
 
-        console.log('1');
+    const result = handleSubmitForm(data, branchSchema);
 
-        const result = handleSubmitForm(data, branchSchema);
+    if (!result || !result.success || result.error) {
+      return;
+    }
 
-        if (!result || !result.success || result.error) {
-            return;
+    const { id, name, address, phone, email } = data;
+
+    console.log(data);
+
+    post(API_ENDPOINTS.BRANCH.LIST, {
+      id,
+      // branchAvt,
+      name,
+      address,
+      phone,
+      email,
+    })
+      .then((res) => {
+        const { data } = res;
+        if (!data.success) {
+          return errorToastHandler(data);
         }
+        // successfully
+        toastSuccess("Create successfully!");
+        navigate("/adminTest/branchList");
+      })
+      .catch((err) => {
+        console.log(err.response);
+        errorToastHandler(err.response);
+      });
+  };
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+    }
+  }, [isSubmitSuccessful, reset]);
 
-        const { id, name, address, phone, email } = data;
-
-        console.log(data);
-
-        post(API_ENDPOINTS.BRANCH.BRANCH, {
-            id,
-            // branchAvt,
-            name,
-            address,
-            phone,
-            email,
-        })
-            .then((res) => {
-                const { data } = res;
-                if (!data.success) {
-                    return errorToastHandler(data);
-                }
-                // successfully
-                toastSuccess("Create successfully!");
-                navigate('/adminTest/branchList');
-            })
-            .catch((err) => {
-                console.log(err.response);
-                errorToastHandler(err.response);
-            });
-    };
-    useEffect(() => {
-        if (isSubmitSuccessful) {
-            reset();
-        }
-    }, [isSubmitSuccessful, reset]);
-
-    return [handleSubmit(onSubmit), isSubmitting, control] as const;
+  return [handleSubmit(onSubmit), isSubmitting, control] as const;
 }
