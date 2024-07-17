1088e36b3573b39f2dc8ea36ebff09f42fddf9eb
Mikocay <huynhtd97@gmail.com>
2024-07-17 21:57:47 +0700

    [DuongHT] handle error at admin, manager, dashboard screen

--- src/pages/Admin/adminDashboard/components/TreatmentPieChart.tsx
+++ src/pages/Admin/adminDashboard/components/TreatmentPieChart.tsx
@@ -1,38 +1,38 @@
-import { PieChart } from '@mui/x-charts/PieChart';
+import { PieChart } from "@mui/x-charts/PieChart";
 
 type TreatmentCategoriesProp = {
-    treatId: number;
-    totalAppointments: number;
-    treatmentName: string;
-}
+  treatId: number;
+  totalAppointments: number;
+  treatmentName: string;
+};
 
 type TreatmentPieChartProps = {
-    treatmentCategories: TreatmentCategoriesProp[];
-}
+  treatmentCategories: TreatmentCategoriesProp[];
+};
 
 function TreatmentPieChart({ treatmentCategories }: TreatmentPieChartProps) {
+  const PieData = treatmentCategories.map((treatment) => ({
+    value: treatment.totalAppointments,
+    label: treatment.treatmentName,
+  }));
 
-    const PieData = treatmentCategories.map((treatment) => ({
-        value: treatment.totalAppointments,
-        label: treatment.treatmentName,
-    }));
-
-    return (
-        <PieChart
-            series={[
-                {
-                    data: PieData,
-                    innerRadius: 30,
-                    outerRadius: 100,
-                    paddingAngle: 5,
-                    cornerRadius: 5,
-                    highlightScope: { faded: "global", highlighted: "item" },
-                    faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
-                }
-            ]}
-            height={300}
-        />
-    );
+  return (
+    <PieChart
+      series={[
+        {
+          data: PieData,
+          cx: 170,
+          innerRadius: 30,
+          outerRadius: 100,
+          paddingAngle: 5,
+          cornerRadius: 5,
+          highlightScope: { faded: "global", highlighted: "item" },
+          faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
+        },
+      ]}
+      height={300}
+    />
+  );
 }
 
 export default TreatmentPieChart;
--- src/pages/Admin/adminUpdate/EmployeeUpdate/EmployeeUpdateBody.tsx
+++ src/pages/Admin/adminUpdate/EmployeeUpdate/EmployeeUpdateBody.tsx
@@ -1,144 +1,127 @@
-import Box from '@mui/material/Box';
-import '@/styles/globals.css'
-import Grid from '@mui/material/Grid';
-import useEmployeeUpdate from './useEmployeeUpdate';
-import FormInputText from '@/components/Form/FormInputText';
-import FormSelect from '@/components/Form/FormSelect';
-import MyButton from '@/components/MyButton';
-import { useParams } from 'react-router-dom';
-import { API_ENDPOINTS } from '@/utils/api';
-import axios from '@/config/axios';
-import { useEffect } from 'react';
+import Box from "@mui/material/Box";
+import "@/styles/globals.css";
+import Grid from "@mui/material/Grid";
+import useEmployeeUpdate from "./useEmployeeUpdate";
+import FormInputText from "@/components/Form/FormInputText";
+import FormSelect from "@/components/Form/FormSelect";
+import MyButton from "@/components/MyButton";
+import { useParams } from "react-router-dom";
+import { API_ENDPOINTS } from "@/utils/api";
+import axios from "@/config/axios";
+import { useEffect } from "react";
 
 function EmployeeUpdateBody() {
+  const [handleSubmit, isSubmitting, control, setValues] = useEmployeeUpdate();
+  const { id } = useParams<{ id: string }>();
 
-    const [handleSubmit, isSubmitting, control, setValues] = useEmployeeUpdate();
-    const { id } = useParams<{ id: string }>();
+  const getOneCourse = async (id: string) => {
+    try {
+      const res = await axios.get(`${API_ENDPOINTS.USERS.USER}/${id}`);
+      if (res.status === 200) {
+        const employeeData = res.data.data;
+        setValues(employeeData);
+        console.log(employeeData);
+      }
+    } catch (error) {
+      console.error("Error fetching employee data:", error);
+    }
+  };
 
-    const getOneCourse = async (id: string) => {
-        try {
-            const res = await axios.get(`${API_ENDPOINTS.USERS.USER}/${id}`);
-            if (res.status === 200) {
-                const employeeData = res.data.data;
-                setValues(employeeData);
-                console.log(employeeData);
-            }
-        } catch (error) {
-            console.error("Error fetching employee data:", error);
-        }
-    };
+  useEffect(() => {
+    if (id) getOneCourse(id);
+  }, [id]);
 
-    useEffect(() => {
-        if (id) getOneCourse(id);
-    }, [id]);
+  return (
+    <Box component="form" noValidate onSubmit={handleSubmit}>
+      <Grid
+        container
+        // component=''
+        className="create-screen"
+        spacing={10}
+      >
+        <Grid item xs={false} sm={false} md={6}>
+          <FormInputText
+            control={control}
+            name="email"
+            outsideLabel=""
+            required
+            fullWidth
+            label="Mail"
+            autoFocus
+            disabled
+          />
+          <FormInputText
+            control={control}
+            name="username"
+            outsideLabel=""
+            required
+            fullWidth
+            label="Username"
+            autoFocus
+          />
+          <FormInputText
+            control={control}
+            name="phone"
+            outsideLabel=""
+            required
+            fullWidth
+            label="Mobile Phone"
+            autoFocus
+          />
 
-    return (
-        <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-        >
-            <Grid
-                container
-                // component=''
-                className='create-screen'
-                spacing={10}
-            >
-                <Grid
-                    item
-                    xs={false}
-                    sm={false}
-                    md={6}
-                >
-                    <FormInputText
-                        control={control}
-                        name="username"
-                        outsideLabel=""
-                        required
-                        fullWidth
-                        label="Username"
-                        autoFocus
-                    />
-                    <FormInputText
-                        control={control}
-                        name="phone"
-                        outsideLabel=""
-                        required
-                        fullWidth
-                        label="Mobile Phone"
-                        autoFocus
-                    />
-                    <FormInputText
-                        control={control}
-                        name="email"
-                        outsideLabel=""
-                        required
-                        fullWidth
-                        label="Mail"
-                        autoFocus
-                    />
-                    <FormSelect
-                        name="gender"
-                        control={control}
-                        label="Gender"
-                        options={[
-                            { value: true, label: 'Male' },
-                            { value: false, label: 'Female' },
-                        ]}
-                    />
-                </Grid>
-                <Grid
-                    item
-                    xs={false}
-                    sm={false}
-                    md={6}
-                >
-                    <FormInputText
-                        control={control}
-                        name="lastName"
-                        outsideLabel=""
-                        required
-                        fullWidth
-                        label="Last Name"
-                        autoFocus
-                    />
-                    <FormInputText
-                        control={control}
-                        name="firstName"
-                        outsideLabel=""
-                        required
-                        fullWidth
-                        label="First Name"
-                        autoFocus
-                    />
-                    <FormInputText
-                        control={control}
-                        name="address"
-                        outsideLabel=""
-                        required
-                        fullWidth
-                        label="Address"
-                        autoFocus
-                    />
-                </Grid>
-            </Grid>
-            <Box
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'left',
-                    m: 1
-                }}
-            >
-                <MyButton
-                    type="submit"
-                    variant="contained"
-                    disabled={isSubmitting}
-                >
-                    Update
-                </MyButton>
-            </Box>
-        </Box >
-    );
+          <FormSelect
+            name="gender"
+            control={control}
+            label="Gender"
+            options={[
+              { value: true, label: "Male" },
+              { value: false, label: "Female" },
+            ]}
+          />
+        </Grid>
+        <Grid item xs={false} sm={false} md={6}>
+          <FormInputText
+            control={control}
+            name="lastName"
+            outsideLabel=""
+            required
+            fullWidth
+            label="Last Name"
+            autoFocus
+          />
+          <FormInputText
+            control={control}
+            name="firstName"
+            outsideLabel=""
+            required
+            fullWidth
+            label="First Name"
+            autoFocus
+          />
+          <FormInputText
+            control={control}
+            name="address"
+            outsideLabel=""
+            required
+            fullWidth
+            label="Address"
+            autoFocus
+          />
+        </Grid>
+      </Grid>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "left",
+          my: 1,
+        }}
+      >
+        <MyButton type="submit" variant="contained" disabled={isSubmitting}>
+          Update
+        </MyButton>
+      </Box>
+    </Box>
+  );
 }
 
 export default EmployeeUpdateBody;
